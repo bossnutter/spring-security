bdd11b5f2b940167bf44458c2493bf57f7159279
Chris Crabtree <chris.crabtree@matillion.com>
2021-11-12 16:21:18 +0000

    Initial commit.

---
+++ src/main/java/com/bossnutter/springsecdemo/SpringSecDemoApplication.java
@@ -0,0 +1,15 @@
+package com.bossnutter.springsecdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecDemoApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SpringSecDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bossnutter/springsecdemo/SpringSecDemoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.bossnutter.springsecdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecDemoApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
